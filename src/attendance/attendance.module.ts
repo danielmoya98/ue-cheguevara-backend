b4496a7fc04bc13fa1b26345148b7789ae035e92
danielmoya98 <moyacarlos09@gmail.com>
2026-04-18 11:01:07 -0400

    asiistencia fix

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { IdentityModule } from '../identity/identity.module'; // Importamos Identity para usar el validador QR
+import { IdentityModule } from '../identity/identity.module'; 
 import { AttendanceCronService } from './attendance.cron';
 
 @Module({
   imports: [PrismaModule, IdentityModule],
   controllers: [AttendanceController],
-  providers: [AttendanceService],
-  exports: [AttendanceService, AttendanceCronService],
+  providers: [AttendanceService, AttendanceCronService],
+  exports: [AttendanceService], // 🔥 Corregido: Faltaba el corchete aquí
 })
 export class AttendanceModule {}
