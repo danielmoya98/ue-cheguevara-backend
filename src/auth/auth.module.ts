de8b30a5bb7dd6855bc9649227ba31402004d1bc
danielmoya98 <moyacarlos09@gmail.com>
2026-04-26 02:32:16 -0400

    refactor auth2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
