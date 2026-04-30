fc6a17ed4b947dc2608f584493affb9a73051408
danielmoya98 <moyacarlos09@gmail.com>
2026-04-30 13:31:17 -0400

    fix controllers 3

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { EnrollmentsService } from './enrollments.service';
 import { EnrollmentsController } from './enrollments.controller';
 import { EncryptionService } from '../common/services/encryption.service'; // 🔥 IMPORTADO
-import { EnrollmentsPolicy } from './enrollments.policy'; // 🔥 POLÍTICA ABAC IMPORTADA
 
 @Module({
   controllers: [EnrollmentsController],
   providers: [
     EnrollmentsService,
     EncryptionService, // 🔥 INYECTADO
-    EnrollmentsPolicy, // 🔥 INYECTADO
   ],
 })
 export class EnrollmentsModule {}
