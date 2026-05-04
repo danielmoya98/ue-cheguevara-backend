3209990d7be7ee1b0e9cad4ab10cdb8dbc081ef3
danielmoya98 <moyacarlos09@gmail.com>
2026-05-03 23:00:38 -0400

    fix!1

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,10 +1,22 @@
+// src/reports/reports.module.ts
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
-import { ReportsGateway } from './gateways/reports/reports.gateway';
+import { ReportsProcessor } from './reports.processor'; // 🔥 No olvides registrar el Worker
+import { PrismaModule } from '../prisma/prisma.module';
+import { TimetablesModule } from '../timetables/timetables.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    TimetablesModule,
+    BullModule.registerQueue({
+      name: 'reports-queue',
+    }),
+  ],
   controllers: [ReportsController],
-  providers: [ReportsService, ReportsGateway],
+  providers: [ReportsService, ReportsProcessor],
+  exports: [ReportsService],
 })
 export class ReportsModule {}
